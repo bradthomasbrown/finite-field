e6c26074661cf8e444313706a348dac1bf4cae2f
Brad Brown <bradthomasbrown@outlook.com>
2025-12-12 18:24:48 -0600

    some doc changes, additional methods `divide`, `power`, and `sqrt_p3mod4`

--- src/finite-field.ts
+++ src/finite-field.ts
@@ -4,14 +4,14 @@
 class FiniteField {
 
     /**
-     * The number of elements in a finite field.
+     * The order of a finite field.
      * @type {bigint}
      */
     p:bigint
 
     /**
      * Creates an instance of a finite field.
-     * @param {bigint|number} p - The number of elements in a finite field.
+     * @param {bigint|number} p - The order of a finite field.
      */
     constructor(p:bigint)
     constructor(p:number)
@@ -59,7 +59,7 @@ class FiniteField {
     }
 
     /**
-     * Return the multiplicative inverse of an integer in this finite field.
+     * Return the multiplicative inverse of an integer over this instance of a finite field.
      * @param {bigint|number} a - The integer to find the multiplicative inverse of.
      * @returns {bigint} The additive inverse.
      */
@@ -68,7 +68,38 @@ class FiniteField {
     }
 
     /**
+     * Divide two numbers over this instance of a finite field.
+     * @param {bigint|number} a - The dividend or first operator of a division operation.
+     * @param {bigint|number} b - The divisor or second operator of a division operation.
+     * @returns {bigint} The quotient of the dividend and divisor over this instance of a finite field.
+     */
+    divide(a:bigint|number, b:bigint|number):bigint {
+        return FiniteField.divide(this.p, a, b);
+    }
+
+    /**
+     * Raise a base to the power of an exponent over this finite field.
+     * @param {bigint|number} b - The base or first operator of an exponentiation operation.
+     * @param {bigint|number} e - The exponent or second operator of a exponentiation operation.
+     * @returns {bigint} The power of the base and exponent over this instance of a finite field.
+     */
+    power(b:bigint|number, e:bigint|number):bigint {
+        return FiniteField.power(this.p, b, e);
+    }
+
+    /**
+     * Calculate the square root of a value over this instance of a finite field.
+     * - Note: This function assumes the order of the field is equivalent to 3 mod 4 and that value `a` is a quadratic residue over this instance of a finite field.
+     * @param {bigint|number} a - The value to calculate the square root of.
+     * @returns {bigint} The square root of the value over this instance of a finite field.
+     */
+    sqrt_p3mod4(a:bigint|number):bigint {
+        return FiniteField.sqrt_p3mod4(this.p, a);
+    }
+
+    /**
      * Add two numbers over a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
      * @param {bigint|number} a - The augend or first operator of an addition operation.
      * @param {bigint|number} b - The addend or second operator of an addition operation.
      * @returns {bigint} The sum of the augend and addend over a finite field of order `p`.
@@ -79,6 +110,7 @@ class FiniteField {
 
     /**
      * Multiply two numbers over a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
      * @param {bigint|number} a - The multiplier or first operator of a multiplication operation.
      * @param {bigint|number} b - The multiplicand or second operator of a multiplication operation.
      * @returns {bigint} The product of the multiplier and multiplicand over a finite field of order `p`.
@@ -89,6 +121,7 @@ class FiniteField {
 
     /**
      * Return the additive inverse of an integer in a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
      * @param {bigint|number} a - The integer to find the additive inverse of.
      * @returns {bigint} The additive inverse.
      */
@@ -98,6 +131,7 @@ class FiniteField {
 
     /**
      * Subtract two numbers over a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
      * @param {bigint|number} a - The minuend or first operator of a subtraction operation.
      * @param {bigint|number} b - The subtrahend or second operator of a subtraction operation.
      * @returns {bigint} The difference of the minuend and subtrahend over a finite field of order `p`.
@@ -108,6 +142,7 @@ class FiniteField {
 
     /**
      * Return the multiplicative inverse of an integer in a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
      * @param {bigint|number} a - The integer to find the multiplicative inverse of.
      * @returns {bigint} The additive inverse.
      */
@@ -121,6 +156,65 @@ class FiniteField {
         const passes = (k * a - 1n) / rate;
         const result = (p * passes + 1n) / a;
         return result;
+    }
+
+    /**
+     * Divide two numbers over a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
+     * @param {bigint|number} a - The dividend or first operator of a division operation.
+     * @param {bigint|number} b - The divisor or second operator of a division operation.
+     * @returns {bigint} The quotient of the dividend and divisor over a finite field of order `p`.
+     */
+    static divide(p:bigint|number, a:bigint|number, b:bigint|number):bigint {
+        p = BigInt(p);
+        a = BigInt(a);
+        b = BigInt(b);
+        const reciprocalB = FiniteField.reciprocal(p, b);
+        return FiniteField.multiply(p, a, reciprocalB);
+    }
+
+    /**
+     * Raise a base to the power of an exponent over a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
+     * @param {bigint|number} b - The base or first operator of an exponentiation operation.
+     * @param {bigint|number} e - The exponent or second operator of a exponentiation operation.
+     * @returns {bigint} The power of the base and exponent over a finite field of order `p`.
+     */
+    static power(p:bigint|number, b:bigint|number, e:bigint|number):bigint {
+        p = BigInt(p);
+        b = BigInt(b);
+        e = BigInt(e);
+        let _1e_ = 1n;
+        for (; e > 0n; e >>= 1n, b = FiniteField.multiply(p, b, b))
+            if ((e & 1n) != 0n) _1e_ = FiniteField.multiply(p, _1e_, b);
+        return _1e_;
+    }
+
+    /**
+     * Return whether or not a value is a quadratic residue over a finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
+     * @param {bigint|number} a - The value to check.
+     * @returns {boolean} True if the value is a quadratic residue, false otherwise.
+     */
+    static qr(p:bigint|number, a:bigint|number):boolean {
+        p = BigInt(p);
+        a = BigInt(a);
+        const e = p - 1n >> 1n;
+        return FiniteField.power(p, a, e) == 1n;
+    }
+
+    /**
+     * Calculate the square root of a value over a finite field of order `p`.
+     * - Note: This function assumes the order of the field is equivalent to 3 mod 4 and that value `a` is a quadratic residue over the finite field of order `p`.
+     * @param {bigint|number} p - The order of the finite field.
+     * @param {bigint|number} a - The value to calculate the square root of.
+     * @returns {bigint} The square root of the value.
+     */
+    static sqrt_p3mod4(p:bigint|number, a:bigint|number):bigint {
+        p = BigInt(p);
+        a = BigInt(a);
+        const e = p + 1n >> 2n;
+        return FiniteField.power(p, a, e);
     }
 
 }
